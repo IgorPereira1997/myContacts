207fb4dea1d5a305478fb064fefdfdf25301c148
Igor Pereira <pereira.t.igor@gmail.com>
2023-10-23 21:52:44 -0300

    Added eslint and editorconfig and their config to project

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-    response.send('Hello World!');
-})
+  response.send('Hello World!');
+});
 
-app.listen(3000, () => console.log("👉 Server initialized at http://localhost:3000! 👈"));
+app.listen(3000, () => console.log('👉 Server initialized at http://localhost:3000! 👈'));
