cd075c369b8014c9d452edb3ab292b2f82cd856a
Igor Pereira <pereira.t.igor@gmail.com>
2023-10-25 15:04:46 -0300

    Refactored CRUD for using database instead of array

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -1,5 +1,5 @@
-const { response } = require("express");
 const ContactsRepository = require('../repositories/ContactsRepository');
+
 class ContactContoller {
   // Listar todos os registros
   async index(request, response) {
@@ -7,83 +7,78 @@ class ContactContoller {
     const contacts = await ContactsRepository.findAll(orderBy);
 
     response.json(contacts);
-    //response.send('Greetings from ContactController (index)!');
+    // response.send('Greetings from ContactController (index)!');
   }
 
-  //Obter UM registro
+  // Obter UM registro
   async show(request, response) {
     const { id } = request.params;
     const contact = await ContactsRepository.findById(id);
 
-    if(!contact){
-      return response.status(404).json({ error: "Contact not found"});
+    if (!contact) {
+      return response.status(404).json({ error: 'Contact not found' });
     }
 
-    response.json(contact);
+    return response.json(contact);
   }
 
-  //Criar novo registro
+  // Criar novo registro
   async store(request, response) {
-    const { name, email, phone, category_id} = request.body;
+    const {
+      name, email, phone, category_id,
+    } = request.body;
 
-    if(!name){
-      return response.status(400).json({ error: "Name is required"});
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
     }
 
     const contactExists = await ContactsRepository.findByEmail(email);
 
-    if(contactExists){
-      return response.status(400).json({ error: "Email is already taken"});
+    if (contactExists) {
+      return response.status(400).json({ error: 'Email is already taken' });
     }
 
     const contact = await ContactsRepository.create({
-      name, email, phone, category_id
+      name, email, phone, category_id,
     });
 
-    response.json(contact);
-
+    return response.json(contact);
   }
 
-  //Editar UM registro
+  // Editar UM registro
   async update(request, response) {
     const { id } = request.params;
-    const { name, email, phone, category_id} = request.body;
+    const {
+      name, email, phone, category_id,
+    } = request.body;
 
     const contactExists = await ContactsRepository.findById(id);
-    if(!contactExists){
-      return response.status(404).json({ error: "Contact not found"});
+    if (!contactExists) {
+      return response.status(404).json({ error: 'Contact not found' });
     }
 
-    if(!name){
-      return response.status(400).json({ error: "Name is required"});
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
     }
 
     const emailTaken = await ContactsRepository.findByEmail(email);
-    if(emailTaken && emailTaken.id !== id){
-      return response.status(400).json({ error: "Email is in use"});
+    if (emailTaken && emailTaken.id !== id) {
+      return response.status(400).json({ error: 'Email is in use' });
     }
 
     const contact = await ContactsRepository.update(id, {
-        name, email, phone, category_id
+      name, email, phone, category_id,
     });
 
-    response.json(contact);
+    return response.json(contact);
   }
 
-  //Deletar um registro
+  // Deletar um registro
   async delete(request, response) {
-
     const { id } = request.params;
-    const contact = await ContactsRepository.findById(id);
-
-    if(!contact){
-      return response.status(404).json({ error: "Contact not found"});
-    }
-
     await ContactsRepository.delete(id);
     // Status: No Content (204)
-    response.sendStatus(204);
-
+    return response.sendStatus(204);
   }
 }
 
--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,39 +1,25 @@
-const { v4 } = require('uuid');
-
 const db = require('../../database');
 
-let contacts = [
-  {
-    id: v4(),
-    name: 'Igor',
-    email: "igor@teste.com",
-    phone: '1234567890',
-    category_id: v4(),
-  },
-  {
-    id: v4(),
-    name: 'Luiz',
-    email: "luiz@teste.com",
-    phone: '0987654321',
-    category_id: v4(),
-  },
-];
-
-class ContactsRepository{
-  async findAll(orderBy = "ASC") {
+class ContactsRepository {
+  async findAll(orderBy = 'ASC') {
     const direction = orderBy.toUpperCase() === 'DESC' ? 'DESC' : 'ASC';
     const rows = await db.query(`SELECT * FROM contacts ORDER BY name ${direction}`);
     return rows;
   }
-  async findById(id){
-    const [row] = await db.query(`SELECT * FROM contacts WHERE id = $1`, [id]);
+
+  async findById(id) {
+    const [row] = await db.query('SELECT * FROM contacts WHERE id = $1', [id]);
     return row;
   }
-  async findByEmail(email){
-    const [row] = await db.query(`SELECT * FROM contacts WHERE email = $1`, [email]);
+
+  async findByEmail(email) {
+    const [row] = await db.query('SELECT * FROM contacts WHERE email = $1', [email]);
     return row;
   }
-  async create({ name, email, phone, category_id}){
+
+  async create({
+    name, email, phone, category_id,
+  }) {
     const [row] = await db.query(`
     INSERT INTO contacts(name, email, phone, category_id)
     VALUES ($1, $2, $3, $4) RETURNING *;
@@ -41,30 +27,30 @@ class ContactsRepository{
 
     return row;
   }
-  update(id, { name, email, phone, category_id }){
-    return new Promise((resolve, reject) => {
-      const updatedContact = {
-        id,
-        name,
-        email,
-        phone,
-        category_id
-      };
 
-      contacts = contacts.map((contact) => (
-        contact.id === id ? updatedContact : contact
-      ));
-
-      resolve(updatedContact);
-    });
+  async update(id, {
+    name, email, phone, category_id,
+  }) {
+    const [row] = await db.query(`
+    UPDATE contacts
+    SET
+      name = $1,
+      email = $2,
+      phone = $3,
+      category_id = $4
+    WHERE id = $5
+    RETURNING *
+    `, [name, email, phone, category_id, id]);
+    return row;
   }
-  delete(id){
-    return new Promise((resolve, reject) => {
-      contacts = contacts.filter((contact) => contact.id !== id);
-      resolve();
-    });
+
+  async delete(id) {
+    const deleteOp = await db.query(`
+    DELETE FROM contacts WHERE id = $1
+    `, [id]);
+    return deleteOp;
   }
 }
 
-//Singleton
+// Singleton
 module.exports = new ContactsRepository();
