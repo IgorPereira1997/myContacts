c58927c90f197d90156284060b9a17b939abf6a7
Igor Pereira <pereira.t.igor@gmail.com>
2023-10-25 19:15:37 -0300

    Added routes to categories page

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,20 @@
 const { Router } = require('express');
 
-const ContactContoller = require('./app/controllers/ContactController');
+const ContactController = require('./app/controllers/ContactController');
+const CategoriesController = require('./app/controllers/CategoryController');
 
 const router = Router();
 
-router.get('/contacts', ContactContoller.index);
-router.get('/contacts/:id', ContactContoller.show);
-router.delete('/contacts/:id', ContactContoller.delete);
-router.post('/contacts', ContactContoller.store);
-router.put('/contacts/:id', ContactContoller.update);
+router.get('/contacts', ContactController.index);
+router.get('/contacts/:id', ContactController.show);
+router.delete('/contacts/:id', ContactController.delete);
+router.post('/contacts', ContactController.store);
+router.put('/contacts/:id', ContactController.update);
+
+router.get('/categories', CategoriesController.index);
+router.get('/categories/:id', CategoriesController.show);
+router.delete('/categories/:id', CategoriesController.delete);
+router.post('/categories', CategoriesController.store);
+router.put('/categories/:id', CategoriesController.update);
 
 module.exports = router;
